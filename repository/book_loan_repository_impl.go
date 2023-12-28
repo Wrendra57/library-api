2df4ae8d2278c8161d0819245f2b6aaa15735da9
Wrendra57 <89427507+Wrendra57@users.noreply.github.com>
2023-12-28 20:49:23 +0700

    .

---
+++ repository/book_loan_repository_impl.go
@@ -0,0 +1,23 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/be/perpustakaan/model/domain"
+)
+
+type BookLoanRepositoryImpl struct {
+}
+
+func NewBookLoanRepository() BookLoanRepository {
+	return &BookLoanRepositoryImpl{}
+}
+
+func (r *BookLoanRepositoryImpl) Create(ctx context.Context, tx *sql.Tx, loan domain.Loan) domain.Loan {
+	SQL:= `
+		insert into book_loan(checkout_date, due_date, status, book_id, user_id, admin_id)
+	`
+	
+	panic("das")
+}
